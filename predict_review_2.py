ea11e77e3c38e8424c6c2174125285c2edd57009
bharathsivakumar <bharathsivakumar@users.noreply.github.com>
2020-03-18 23:51:37 +0530

    Update predict_review_2.py
    In this updated version of the predict_review_2 function, we have added an extra function that calculates the confusion matrix for the testing set and also moved the plotting function to another function called plot_learning_curves the other function we are creating is called stimate_learning_curves_Naive_Bayes that we use to train our estimator and the accuracy of the model as a list with increasing training dataset size

--- predict_review_2.py
+++ predict_review_2.py
@@ -1,24 +1,28 @@
 import pandas as pd
 import numpy as np 
-import random 
-from nltk import classify 
 from nltk import NaiveBayesClassifier
-from nltk.tokenize import word_tokenize
 from nltk.metrics import ConfusionMatrix
-from sklearn.model_selection import learning_curve 
 from sklearn import metrics 
 import matplotlib.pyplot as plt 
-from matplotlib.ticker import FuncFormatter
+import pickle 
 
-def plot_learning_curves_Naive_Bayes(training_data_df, testing_data_df,suptitle='Learning curve of a Naive Bayes Classifier', title='Estimating sentiment of a movie review', xlabel='Size of training dataset', ylabel='Accuracy'):
+def estimate_learning_curves_Naive_Bayes(training_data_df, testing_data_df):
     """
-    Plots learning curves for a Naive Bayes estimator
+    Returns the final trained Naive Bayes estimator
+    trained on the entier training set as well as 
+    the accuracy of the model on the training and test 
+    datasets with increasing training set size 
+    as well as the predicted labels for the training dataset as a list 
+    and the actual labels of the training dataset as a list 
+
     Parameters
     ----------
     training_data_df : pd.DataFrame
         training set with labels where features are in 
+        dictionary form with the words as keys and true 
+        as the value for each key 
     testing_data_df : pd.DataFrame
-        training set (response)
+        testing set in the same format
     suptitle : str
         Chart suptitle
     title: str
@@ -29,19 +33,28 @@ def plot_learning_curves_Naive_Bayes(training_data_df, testing_data_df,suptitle=
         Label for the y axis
     Returns
     -------
-    Plot of learning curves
+    final trained Naive Bayes estimator 
+    the accuracies of the model on the training set as a list  
+    the accuracies of the model on the test set as a list  
     """
     
     # create lists to store train and testing scores
     train_score = []
     test_score = []
+
+    #converting the training set data frame and testing set dataframe
+    #into list since that is the format in which NLTK Naive Bayes 
+    # classifier takes its input as 
+
     training_data_list = training_data_df.values.tolist()
     testing_data_list = testing_data_df.values.tolist()	
     # create ten incremental training set sizes
     training_set_sizes = np.linspace(5, len(training_data_list), num = 10, dtype='int')
+    
+    #converting the labels for the testing dataset into a list since 
+    #we need in this form to evaluate the accuracy of our classifier 
+    #using the sklearn metric 
 
-    training_data_list = training_data_df.values.tolist()
-    testing_data_list = testing_data_df.values.tolist()
     labels_testing_actual_list = list(testing_data_df.iloc[:,1].values)
     # for each one of those training set sizes
 
@@ -53,15 +66,38 @@ def plot_learning_curves_Naive_Bayes(training_data_df, testing_data_df,suptitle=
         labels_training_actual_list = list(training_data_df.iloc[0:i,1].values)
         labels_testing_predicted_list = list(testing_data_df.iloc[:,0].apply(classifier.classify).values)
         train_accuracy = metrics.accuracy_score(labels_training_actual_list,labels_training_predicted_list)
-        # calculate the testing accuracy using the whole testing set
-        
+        # calculate the testing accuracy using the whole testing set 
 	
         test_accuracy = metrics.accuracy_score(labels_testing_predicted_list,labels_testing_actual_list)
         # store the scores in their respective lists
         train_score.append(train_accuracy)
         test_score.append(test_accuracy)
 
-    # plot learning curves
+    return classifier, training_set_sizes, train_score, test_score, labels_testing_predicted_list, labels_testing_actual_list
+
+# plot learning curves
+def plot_learning_curves(train_score, test_score, training_set_sizes, suptitle='Learning curve of a Naive Bayes Classifier', title='Estimating sentiment of a movie review', xlabel='Size of training dataset', ylabel='Accuracy'):
+
+    """
+    Returns the plot of the learning curve 
+    as training accuracy and test accuracy 
+    with increasing size of the training set
+
+    Parameters
+    ----------
+    training_score : list
+        training accuracies as a list 
+    testing_score : list
+        testing accuracies as a list 
+    training_set_sizes : np.array
+        an array containg the set of training sizes 
+        for which the accuracies will be plotted 
+    Returns
+    -------
+    Returns the plot of the learning curve 
+    as training accuracy and test accuracy 
+    with increasing size of the training set
+    """
     fig, ax = plt.subplots(figsize=(14, 9))
     ax.plot(training_set_sizes, train_score, c='gold')
     ax.plot(training_set_sizes, test_score, c='steelblue')
@@ -75,24 +111,10 @@ def plot_learning_curves_Naive_Bayes(training_data_df, testing_data_df,suptitle=
     ax.tick_params(axis='both', labelsize=12)
     ax.set_ylim(0, 1)
 
-    def percentages(x, pos):
-        """The two args are the value and tick position"""
-        if x < 1:
-            return '{:1.0f}'.format(x*100)
-        return '{:1.0f}%'.format(x*100)
-
-    def numbers(x, pos):
-        """The two args are the value and tick position"""
-        if x >= 1000:
-            return '{:1,.0f}'.format(x)
-        return '{:1.0f}'.format(x)
-
-    y_formatter = FuncFormatter(percentages)
-    ax.yaxis.set_major_formatter(y_formatter)
-
-    x_formatter = FuncFormatter(numbers)
-    ax.xaxis.set_major_formatter(x_formatter)
     plt.show()
+
+def confusion_matrix_Naive_Bayes(labels_testing_predicted_list, labels_testing_actual_list):
+    return ConfusionMatrix(labels_testing_predicted_list, labels_testing_actual_list)
 
 data_model_df = pd.read_pickle('model_data.pickle')
 
@@ -105,4 +127,14 @@ data_model_df = data_model_df.sample(frac=1).reset_index(drop=True) #specifying 
 training_data_df = data_model_df[:training_size]
 testing_data_df  =  data_model_df[training_size:]
 
-plot_learning_curves_Naive_Bayes(training_data_df, testing_data_df)
+classifier, training_set_sizes, train_score, test_score,labels_testing_predicted_list, labels_testing_actual_list = estimate_learning_curves_Naive_Bayes(training_data_df, testing_data_df)
+
+#We are now going to save our fully trained classifier into a pickle file
+save_classifier = open("naive_bayes_sentiment_analysis.pickle","wb")
+pickle.dump(classifier, save_classifier)
+save_classifier.close()
+
+cm = confusion_matrix_Naive_Bayes(labels_testing_predicted_list, labels_testing_actual_list)
+print("\nConfusion matrix on the testing dataset for our naive bayes classifier is:\n", cm)
+
+plot_learning_curves(train_score, test_score, training_set_sizes)
